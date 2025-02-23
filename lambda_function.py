8d5eb0ea674f594f7967d4a81f50ed427545f326
unknown <jarrry.c@gmail.com>
2025-02-24 00:05:51 +1100

    Reading secret from aws env

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,10 @@
 import json
+import os
 
 def lambda_handler(event, context):
+    my_secret = os.getenv("MY_SECRET")
     body = event['body']
+    body['my_secret'] = my_secret
     return {
         'statusCode': 200,
         'body': json.dumps(body)
